deefc5725d222919d6397afd09c6a1f0283fc0f0
Austin Amani <amaniaustin123@gmail.com>
2023-01-16 15:29:18 +0300

    Got documents from the firestore database

--- main.js
+++ main.js
@@ -1,21 +1,70 @@
+// #1 SETTING UP FIRESTORE
+
+// Import the functions you need from the SDK's you need
+import { initializeApp } from 'firebase/app';
 import './style.css';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDocs, getFirestore, collection } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyDQcxo7VfeVspun-YO0rX8Y6HJVz1yuy_E',
-	authDomain: 'fir-firestore-tutorial-c3bf0.firebaseapp.com',
-	projectId: 'fir-firestore-tutorial-c3bf0',
-	storageBucket: 'fir-firestore-tutorial-c3bf0.appspot.com',
-	messagingSenderId: '219613098745',
-	appId: '1:219613098745:web:5ea0a48c43c0cb93106828',
+	apiKey: 'AIzaSyBDk6kgzyvVZBc4QAqjMvmbxF-r8SBu1YQ',
+	authDomain: 'fir-firestore-tutorial-8f5d5.firebaseapp.com',
+	projectId: 'fir-firestore-tutorial-8f5d5',
+	storageBucket: 'fir-firestore-tutorial-8f5d5.appspot.com',
+	messagingSenderId: '29874342677',
+	appId: '1:29874342677:web:e99939bc71673dfe33f71c',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const db = firebase.firestore();
-db.settings({ timestampInSnapshots: true });
+// Initialize Services
+const db = getFirestore(app);
+
+//Collection Refference
+const colRef = collection(db, 'Cafes');
+
+// #2 GETTING DOCUMENTS
+
+//Html Dom Elements
+const cafeList = document.querySelector('#cafe-list');
+
+//Create element and render cafe
+function renderCafe(doc) {
+	let li = document.createElement('li');
+	let name = document.createElement('span');
+	let city = document.createElement('span');
+
+	li.setAttribute('data-id', doc.id);
+	name.textContent = doc.data().Name;
+	city.textContent = doc.data().City;
+
+	li.appendChild(name);
+	li.appendChild(city);
+
+	cafeList.appendChild(li);
+}
+
+//Get all documents from the cafes collection and display them
+//const snapshot = await getDocs(collection(db, 'Cafes'));
+
+//console.log(snapshot.docs);
+
+//Cycle through the documents and dislay their data
+// snapshot.forEach((doc) => {
+// 	console.log(doc.data());
+// });
+
+//Render
+//snapshot.forEach((doc) => {
+//renderCafe(doc);
+//});
+
+//Render using an IIFE function
+(async function getDocuments() {
+	const snapshot = await getDocs(collection(db, 'Cafes'));
+
+	snapshot.forEach((doc) => {
+		renderCafe(doc);
+	});
+})();
